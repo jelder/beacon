13f07a0fd539e438acb41d8bfbf274760c809f9f
Jacob Elder <jacob@boundless.com>
2015-01-31 16:25:31 -0500

    A little reorg

--- beacon.go
+++ beacon.go
@@ -1,57 +1,71 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/codegangsta/negroni"
 	"github.com/dchest/uniuri"
 	"github.com/garyburd/redigo/redis"
 	"github.com/gorilla/mux"
-	"github.com/mholt/binding"
 	"github.com/phyber/negroni-gzip/gzip"
 	"github.com/rs/cors"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"net/url"
-	"os"
 	"runtime"
 	"time"
 )
 
-const cookieMaxAge = 60 * 60 * 60 * 24 * 30
-
-var (
-	redisPool    = redisSetup(redisConfig())
-	beacon_png   = mustReadFile("assets/beacon.png")
-	multi_script = redis.NewScript(-1, fmt.Sprintf("%s", mustReadFile("assets/multi.lua")))
-	events       = make(chan Event, runtime.NumCPU()*100)
-	version      string
+const (
+	cookieMaxAge = 60 * 60 * 60 * 24 * 30
 )
 
-func mustReadFile(path string) []byte {
-	b, err := ioutil.ReadFile(path)
-	if err != nil {
-		panic(err)
-	}
-	return b
-}
+var (
+	redisPool    *redis.Pool
+	events       chan Event
+	multi_script = redis.NewScript(-1, fmt.Sprintf("%s", mustReadFile("assets/multi.lua")))
+	beacon_png   = mustReadFile("assets/beacon.png")
+)
 
 type Event struct {
 	Object string
 	User   string
 }
 
-type TrackJson struct {
-	Visits  int64 `json:"visits"`
-	Uniques int64 `json:"uniques"`
+func init() {
+	redisPool = redisSetup(redisConfig())
+	events = make(chan Event, runtime.NumCPU()*100)
 }
 
-func (trackJson *TrackJson) FieldMap() binding.FieldMap {
-	return binding.FieldMap{
-		&trackJson.Visits:  "visits",
-		&trackJson.Uniques: "uniques",
-	}
+func main() {
+	fmt.Println("Beacon running on", fmt.Sprintf("%d", runtime.NumCPU()), "CPUs")
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	go track()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		http.Redirect(w, req, "https://www.github.com/jelder/beacon", 302)
+	})
+	r.HandleFunc("/{objectId}.png", beaconHandler)
+	r.HandleFunc("/api/v1/{objectId}", apiHandler).Methods("GET")
+	r.HandleFunc("/api/v1/_multi", apiMultiHandler).Methods("POST")
+	r.HandleFunc("/api/v1/{objectId}", apiWriteHandler).Methods("POST").Queries("key", ENV["SECRET_KEY"])
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./assets/")))
+
+	n := negroni.Classic()
+	n.Use(gzip.Gzip(gzip.DefaultCompression))
+	n.Use(cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+	}))
+	n.UseHandler(r)
+	n.Run(listenAddress())
+}
+
+func beaconHandler(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	objectId := vars["objectId"]
+	events <- Event{objectId, uid(w, req)}
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(beacon_png)
 }
 
 func uid(w http.ResponseWriter, req *http.Request) string {
@@ -62,12 +76,11 @@ func uid(w http.ResponseWriter, req *http.Request) string {
 			uid := fmt.Sprintf("%s", uniuri.New())
 			now := time.Now()
 			new_cookie := &http.Cookie{Name: "uid", Value: uid, MaxAge: cookieMaxAge, Expires: now.Add(cookieMaxAge)}
-			log.Print("Setting new cookie ", new_cookie)
+			fmt.Print("Setting new cookie ", new_cookie)
 			http.SetCookie(w, new_cookie)
 			return uid
 		default:
-			log.Fatal(err)
-			return ""
+			fmt.Println(err)
 		}
 	}
 	return cookie.Value
@@ -79,7 +92,7 @@ func track() {
 
 	for {
 		event := <-events
-		log.Print("Tracking ", event.User, " on ", event.Object)
+		fmt.Print("Tracking ", event.User, " on ", event.Object)
 
 		// http://godoc.org/github.com/garyburd/redigo/redis#hdr-Pipelining
 		conn.Send("MULTI")
@@ -94,179 +107,7 @@ func track() {
 
 		_, err := conn.Do("EXEC")
 		if err != nil {
-			log.Print(err)
+			fmt.Print(err)
 		}
 	}
-}
-
-func beaconHandler(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	objectId := vars["objectId"]
-	events <- Event{objectId, uid(w, req)}
-	w.Header().Set("Content-Type", "image/png")
-	w.Write(beacon_png)
-}
-
-func apiHandler(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	objectId := vars["objectId"]
-	conn := redisPool.Get()
-	defer conn.Close()
-
-	uniques, err := redis.Int64(conn.Do("PFCOUNT", "hll_"+objectId))
-	if err != nil {
-		log.Print(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var migrated_visits, migrated_uniques, visits int64
-	mget, err := redis.Values(conn.Do("MGET", "visits_"+objectId, "uniques_"+objectId, "str_"+objectId))
-	if err != nil {
-		log.Print(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	if _, err := redis.Scan(mget, &migrated_visits, &migrated_uniques, &visits); err != nil {
-		log.Print(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	visits += migrated_visits
-	uniques += migrated_uniques
-
-	apiResponse := TrackJson{Visits: visits, Uniques: uniques}
-	js, _ := json.MarshalIndent(apiResponse, "", "  ")
-	w.Header().Set("Server", "Beacon "+version)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
-}
-
-func apiMultiHandler(w http.ResponseWriter, req *http.Request) {
-	conn := redisPool.Get()
-	defer conn.Close()
-	body, _ := ioutil.ReadAll(req.Body)
-	fmt.Printf("%s\n", body)
-
-	// TODO read from POST body
-	test := []string{"foo", "bar", "baz", "blah"}
-
-	keys := variadicScriptArgs(test)
-	visits, err := redis.Int64(multi_script.Do(conn, keys...))
-	if err != nil {
-		log.Print(err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	// TODO also query unique
-	apiResponse := TrackJson{Visits: visits, Uniques: 1}
-	js, _ := json.MarshalIndent(apiResponse, "", "  ")
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
-}
-
-func variadicScriptArgs(args []string) []interface{} {
-	scriptArgs := make([]interface{}, len(args)+1)
-	scriptArgs[0] = len(args) // First argument is length
-	for i, v := range args {
-		scriptArgs[i+1] = interface{}(v)
-	}
-	return scriptArgs
-}
-
-func apiWriteHandler(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	objectId := vars["objectId"]
-	trackJson := new(TrackJson)
-	if binding.Bind(req, trackJson).Handle(w) {
-		return
-	}
-	fmt.Sprintf("%q\n", trackJson)
-	conn := redisPool.Get()
-	defer conn.Close()
-	_, err := conn.Do("MSET", "uniques_"+objectId, trackJson.Uniques, "visits_"+objectId, trackJson.Visits)
-	if err != nil {
-		log.Print(err)
-	}
-}
-
-func listenAddress() string {
-	string := os.Getenv("PORT")
-	if string == "" {
-		return ":8080"
-	} else {
-		return ":" + string
-	}
-}
-
-func redisConfig() (string, string) {
-	redis_provider := os.Getenv("REDIS_PROVIDER")
-	if redis_provider == "" {
-		redis_provider = "OPENREDIS_URL"
-	}
-	string := os.Getenv(redis_provider)
-	if string != "" {
-		url, err := url.Parse(string)
-		password := ""
-		if err != nil {
-			panic(err)
-		}
-		if url.User != nil {
-			password, _ = url.User.Password()
-		}
-		return url.Host, password
-	} else {
-		return "127.0.0.1:6379", ""
-	}
-}
-
-func redisSetup(server, password string) *redis.Pool {
-	log.Print("Connecting to Redis on ", server, password)
-	return &redis.Pool{
-		MaxIdle:     3,
-		IdleTimeout: 240 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", server)
-			if err != nil {
-				return nil, err
-			}
-			if password != "" {
-				if _, err := c.Do("AUTH", password); err != nil {
-					c.Close()
-					return nil, err
-				}
-			}
-			return c, err
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			return err
-		},
-	}
-}
-
-func main() {
-	log.Print("Beacon " + version + " running on " + fmt.Sprintf("%d", runtime.NumCPU()) + "CPUs")
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	go track()
-
-	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		http.Redirect(w, req, "https://www.github.com/jelder/beacon", 302)
-	})
-	r.HandleFunc("/{objectId}.png", beaconHandler)
-	r.HandleFunc("/api/v1/{objectId}", apiHandler).Methods("GET")
-	r.HandleFunc("/api/v1/_multi", apiMultiHandler).Methods("POST")
-	r.HandleFunc("/api/v1/{objectId}", apiWriteHandler).Methods("POST").Queries("key", os.Getenv("SECRET_KEY"))
-
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./assets/")))
-
-	n := negroni.Classic()
-	n.Use(gzip.Gzip(gzip.DefaultCompression))
-	n.Use(cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},
-	}))
-	n.UseHandler(r)
-	n.Run(listenAddress())
 }
